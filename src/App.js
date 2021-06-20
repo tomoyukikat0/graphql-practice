e2a2a92e94abd8f9efed323961406f01c28707d4
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-06-21 01:03:33 +0900

    add .env

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+console.log(GITHUB_TOKEN)
+
 class App extends Component {
   render() {
     return (
-      <div>hi</div>
+      <div>Hello, GraphQL</div>
     );
   }
 }
