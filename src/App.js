5757601c811a2e762169c3a55874607797231869
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-06-23 02:59:11 +0900

    apply search query and variables

--- src/App.js
+++ src/App.js
@@ -2,21 +2,38 @@ import React, { Component } from 'react';
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client'
-import { ME } from './graphql' 
+import { SEARCH_REPOSITORIES } from './graphql'
 
+const VARIABLES = {
+  first: 5,
+  ater: null,
+  last: null,
+  before: null,
+  query: "フロントエンジニア"
+}
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = VARIABLES
+  }
+
   render() {
+    const { query, first, last, before, after } = this.state 
     return (
       <ApolloProvider client={client}>
         <div>Hello, GraphQL</div>
 
-        <Query query={ME}>
+        <Query 
+          query={SEARCH_REPOSITORIES}
+          variables={{ query, first, last, before, after }}
+        >
+          
           {
             ({ loading, error, data }) => {
               if(loading) return 'Loading...'
               if(error) return `Error! ${error.message}` 
-
-              return <div>{data.user.name}</div>
+              console.log({data})
+              return <div></div>
             }
           }
         </Query>
