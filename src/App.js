1a85b9259ad15a85b1cb0a36063d45b68ba19e74
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-06-23 03:08:35 +0900

    create input form

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import { Query } from 'react-apollo'
 import client from './client'
 import { SEARCH_REPOSITORIES } from './graphql'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   ater: null,
   last: null,
   before: null,
   query: "フロントエンジニア"
 }
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = VARIABLES
+    this.state = DEFAULT_STATE
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({
+      ...DEFAULT_STATE,
+      query: event.target.value
+    })
   }
 
   render() {
     const { query, first, last, before, after } = this.state 
+    console.log({query})
     return (
       <ApolloProvider client={client}>
-        <div>Hello, GraphQL</div>
+        <form>
+          <input value={query} onChange={this.handleChange} />
+        </form>
 
         <Query 
           query={SEARCH_REPOSITORIES}
