71d7cb8c0261f97fbbe917ab1ffabedb5155e098
加藤智之 <katotomoyuki@katoutoshiyukinoMacBook-Pro-2.local>
2021-06-26 03:04:49 +0900

    remove-star

--- src/graphql.js
+++ src/graphql.js
@@ -32,8 +32,22 @@ export const ADD_STAR = gql`
   mutation addStar ($input: AddStarInput!) {
     addStar (input: $input) {
       starrable {
+        id
         viewerHasStarred
       }
     }
   }
 `
+
+export const REMOVE_STAR = gql`
+  mutation removeStar ($input: RemoveStarInput!) {
+    removeStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`
+
+
